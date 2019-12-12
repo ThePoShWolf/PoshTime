757c5cdb15a0d2f1d91b0776a2e1eff017f75302
Anthony Howell <anhowell@epiqglobal.com>
2019-12-11 16:36:08 -0800

    started the working data cmdlets

---
+++ src/public/workingdata/Set-PtWorkingDay.ps1
@@ -0,0 +1,12 @@
+Function Set-PtWorkingDay {
+    [cmdletbinding()]
+    Param (
+        [datetime]$Date
+    )
+    <#
+        $script:PtWorkingTimesheet
+        $script:PtWorkingPayPeriod
+        $script:PwWorkingWeek
+        $script:PtWorkingDay
+    #>
+}
