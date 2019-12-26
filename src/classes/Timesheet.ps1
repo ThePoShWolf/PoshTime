c85b2937152ddb93175b97dd607b997b58fc059a
Anthony Howell <anhowell@epiqglobal.com>
2019-12-26 14:47:25 -0800

    added the path and save methods

--- src/classes/Timesheet.ps1
+++ src/classes/Timesheet.ps1
@@ -1,10 +1,32 @@
 Class PoshTimeTimesheet {
     [PoshTimePayPeriod[]]$PayPeriods
+    [string]$Path
 
     PoshTimeTimesheet(
         [PoshTimePayPeriod[]]$PayPeriods
     ){
         $this.PayPeriods = $PayPeriods
+    }
+
+    PoshTimeTimesheet(
+        [PoshTimePayPeriod[]]$PayPeriods,
+        [string]$Path
+    ){
+        $this.PayPeriods = $PayPeriods
+        $this.Path = $Path
+        if (Test-Path $Path) {
+            $this.SaveFile = Get-Item $Path
+        }
+    }
+
+    Save(){
+        if ($null -ne $this.Path) {
+            $this.ToJson() | Out-File $this.Path
+        }
+    }
+
+    Load(){
+        # TODO
     }
 
     [System.Collections.Specialized.OrderedDictionary] ToJsonObject(){
