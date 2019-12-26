7d6eb26f02d5f397f9a504dcf81083c58fb0b503
Anthony Howell <anhowell@epiqglobal.com>
2019-12-26 15:49:45 -0800

    added the load method

--- src/classes/Timesheet.ps1
+++ src/classes/Timesheet.ps1
@@ -1,6 +1,15 @@
 Class PoshTimeTimesheet {
     [PoshTimePayPeriod[]]$PayPeriods
+    [ValidatePattern('\.json$')]
     [string]$Path
+
+    PoshTimeTimesheet(){}
+
+    PoshTimeTimesheet(
+        [string]$Path
+    ){
+        $this.Path = $Path
+    }
 
     PoshTimeTimesheet(
         [PoshTimePayPeriod[]]$PayPeriods
@@ -22,11 +31,48 @@ Class PoshTimeTimesheet {
     Save(){
         if ($null -ne $this.Path) {
             $this.ToJson() | Out-File $this.Path
+        } else {
+            Throw 'Unable to save. Path is null.'
         }
     }
 
     Load(){
         # TODO
+        $weekdays = 'Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'
+        if ($null -ne $this.Path) {
+            $json = Get-Content $this.Path | ConvertFrom-Json -Depth 8
+            $this.PayPeriods = foreach ($payperiod in $json.PayPeriods) {
+                $week1 = & {
+                    $weekHash = @{}
+                        foreach ($weekDay in $weekDays) {
+                            $day = $payperiod.Week1."$weekDay"
+                            $entries = foreach ($entry in $day.Entries) {
+                                [PoshTimeEntry]::new($entry.Start,$entry.End,$entry.Date)
+                            }
+                            if ($entries.Count -ge 1){
+                                $weekHash[$weekDay] = [PoshTimeDay]::new($entries,[datetime]$($day.Day))
+                            }
+                        }
+                    [PoshTimeWeek]::new($weekHash['Sunday'],$weekHash['Monday'],$weekHash['Tuesday'],$weekHash['Wednesday'],$weekHash['Thursday'],$weekHash['Friday'],$weekHash['Saturday'])
+                }
+                $week2 = & {
+                    $weekHash = @{}
+                        foreach ($weekDay in $weekDays) {
+                            $day = $payperiod.Week2."$weekDay"
+                            $entries = foreach ($entry in $day.Entries) {
+                                [PoshTimeEntry]::new($entry.Start,$entry.End,$entry.Date)
+                            }
+                            if ($entries.Count -ge 1){
+                                $weekHash[$weekDay] = [PoshTimeDay]::new($entries,[datetime]$($day.Day))
+                            }
+                        }
+                    [PoshTimeWeek]::new($weekHash['Sunday'],$weekHash['Monday'],$weekHash['Tuesday'],$weekHash['Wednesday'],$weekHash['Thursday'],$weekHash['Friday'],$weekHash['Saturday'])
+                }
+                [PoshTimePayPeriod]::new($week1,$week2)
+            }
+        } else {
+            Throw 'Unable to load. Path is null.'
+        }
     }
 
     [System.Collections.Specialized.OrderedDictionary] ToJsonObject(){
